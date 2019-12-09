ca4d1551e11be0075cce150702d387f4b0fb8721
18321473057 <ycs19930606>
2019-12-09 21:38:29 +0800

    init

---
+++ src/main/java/org/line/com/GatewayApplication.java
@@ -0,0 +1,20 @@
+package org.line.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+//@EnableZuulServer //高版本 附加很多过滤器
+@EnableZuulProxy //低版本
+@EnableEurekaClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
